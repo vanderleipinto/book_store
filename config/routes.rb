d1a237baeed55c9dbafb8dc8dab43099a33ca206
vanderleipinto <vandecopinto@gmail.com>
2024-01-04 18:19:26 -0300

    feat: Add assemblies on book forms, changed book controller params to receive them

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  
+  root "books#index"
   resources :assemblies
   resources :parts
   resources :accounts
   resources :suppliers
-  root "books#index"
   resources :books
   resources :authors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
