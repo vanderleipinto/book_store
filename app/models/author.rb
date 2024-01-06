ed1077104fc4a252ba6a46247c4ea44e73a178d1
vanderleipinto <vandecopinto@gmail.com>
2024-01-06 18:39:29 -0300

    feat: Add cpf field on author

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
+
+  validates :name, presence: true
+  validates :cpf, presence: true, uniqueness: false
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_06_205248) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_06_211346) do
   create_table "accounts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "account_number"
     t.bigint "supplier_id", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_06_205248) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cpf"
   end
 
   create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
