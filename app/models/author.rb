e8f0b84c90c10f0cc571fc50331efb306742579d
vanderleipinto <vandecopinto@gmail.com>
2024-01-07 09:06:17 -0300

    feat: Add validation to Author.cpf, include gem cpf_cnpj

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,7 +1,16 @@
+
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
 
   validates :name, presence: true
-  validates :cpf, presence: true, uniqueness: false
+  validates :cpf, presence: true, uniqueness: true
+
+  validate :cpf, :validate_cpf? 
+
+  private
+
+  def validate_cpf?
+    errors.add(:cpf, "is invalid") unless CPF.valid?(cpf)
+  end
   
 end
